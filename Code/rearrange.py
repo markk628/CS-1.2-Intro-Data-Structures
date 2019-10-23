5b4e0e2e04aa8b3738be4bfaa77b4368e2c7ba2d
Donghyun <markk628@gmail.com>
2019-10-23 15:55:42 -0700

    wrote code instead of using shuffle function

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,25 +1,28 @@
 import sys
 import random
 
-# empty array
+# empty arrays
 word_list = []
+word_list2 = []
 
-# shuffles the word_list array
+'''
+code first picks a word from word_list randomly
+That word is then added to word_list2
+'''
 def shuffle_list(word_list):
-    random.shuffle(word_list)
-    return word_list
+    while len(word_list) > 0:
+        choice = random.choice(word_list)
+        word_list2.append(choice)
+        word_list.remove(choice)
 
 '''
 user enters the words that will go into the word_list array. The shuffle_list function will then
-shuffle the array and return a shuffled list
+return an array with shuffled words
 '''
 if __name__ == "__main__":
 
     for arg in range(1, len(sys.argv)):
         word_list.append(sys.argv[arg])
 
-    print(word_list)
-    
-    word_list = shuffle_list(word_list)
-
-    print(word_list)
+    shuffle_list(word_list)
+    print(word_list2)

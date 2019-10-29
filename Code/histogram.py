f50b5a4042b8cc3db1cffcc40291efd37d27a91b
Donghyun <markk628@gmail.com>
2019-10-28 21:14:42 -0700

    updated frequency function

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,9 +1,12 @@
+import re
+
 source_text = 'olivertwist.txt'
 
 def scrubbed_words(source_text):
     with open(source_text, 'r') as file:
-        words = file.read().split()
-        return words
+        words = file.read()
+        scrubbed_words = re.sub(r'[^a-zA-Z\s]', '', words)
+        return scrubbed_words.split()
 
 def unique_words(source_text):
     histogram = {}
@@ -16,5 +19,27 @@ def unique_words(source_text):
             histogram[word] = 1
     print(len(histogram))
 
+def list_histogram(source_text):
+   words = scrubbed_words(source_text)
+   histogram = []
+   for word in words:
+       instance = [word, 0]
+       for word2 in words:
+           if word == word2:
+               instance[1] += 1
+       if instance not in histogram:
+           histogram.append(instance)
+   print(histogram) 
 
-unique_words(source_text)
+def dic_histogram(source_text):
+    histogram = {}
+    words = scrubbed_words(source_text)
+    for word in words:
+        if word in histogram:
+            histogram[word] += 1
+        else:
+            histogram[word] = 1
+    for key in list(histogram.keys()):
+        print(key, histogram[key])
+
+dic_histogram(source_text)
